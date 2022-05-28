877fe6e2b706b9f080cc7286cf0bc28e4565647a
jokery.zhou <jokery_zhou@outlook.com>
2022-05-28 15:14:01 +0800

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Jokery_blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
